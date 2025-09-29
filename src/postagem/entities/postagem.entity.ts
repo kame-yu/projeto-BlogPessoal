0bdac0cbb0474d28d896020eb9fc8efdbc6ab733
Eric Silva <erics@genstudents.org>
2025-09-29 18:24:22 -0300

    Atualizando as classes

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,16 +1,18 @@
-import { IsNotEmpty } from 'class-validator'; // Validar se o campo não está vazio
+import { IsNotEmpty } from 'class-validator';
 import {
   Column,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Tema } from '../../tema/entities/tema.entity';
 
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
   @PrimaryGeneratedColumn()
   id: number;
-  @IsNotEmpty() // Validar que o campo não pode ser vazio
+  @IsNotEmpty()
   @Column({ length: 100, nullable: false })
   titulo: string;
 
@@ -20,4 +22,9 @@ export class Postagem {
 
   @UpdateDateColumn()
   data: Date;
+
+  @ManyToOne(() => Tema, (tema) => tema.postagem, {
+    onDelete: 'CASCADE',
+  })
+  tema: Tema;
 }
